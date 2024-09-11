afe8ca1853324ee56d2d63485e23475ea074a61d
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-11 16:18:39 +0200

    11.commit

--- javascript/class-notes/JS-02-Operators/app.js
+++ javascript/class-notes/JS-02-Operators/app.js
@@ -2,59 +2,53 @@
 // *                 ARİTMETİK OPERATÖRLER
 // * =======================================================
 
-
-const cola=50
-const cips=35
-const dondurma=20
+const cola = 50;
+const cips = 35;
+const dondurma = 20;
 
 console.log("cola" + cips + dondurma);
 
-let toplam=cola + cips + dondurma
+let toplam = cola + cips + dondurma;
 
-console.log(toplam);//105
-
+console.log(toplam); //105
 
 //! 1 artir
 
-toplam= toplam + 1
+toplam = toplam + 1;
 
-console.log(toplam);//106
+console.log(toplam); //106
 
-toplam++
+toplam++;
 
-console.log(toplam);//107
+console.log(toplam); //107
 
 //!10 arttir
 
-toplam=toplam+10
+toplam = toplam + 10;
 
-toplam+=10 // toplam=toplam+10 kisa yolu
+toplam += 10; // toplam=toplam+10 kisa yolu
 
-console.log(toplam);//127
-
+console.log(toplam); //127
 
 //? + operatörü string concatination islemi de yapar
 
-const sayi1="12"
-const sayi2=45
+const sayi1 = "12";
+const sayi2 = 45;
 
-console.log(sayi1+sayi2);//1245
-console.log(sayi1-sayi2);//-33
+console.log(sayi1 + sayi2); //1245
+console.log(sayi1 - sayi2); //-33
 
+const isim = "mehmet";
+const soyisim = "kara";
 
-const isim="mehmet"
-const soyisim="kara"
-
-console.log("benim adim soyadim " +isim + "" +soyisim);
-
+console.log("benim adim soyadim " + isim + "" + soyisim);
 
 //!BACKTICK (template literals) ````````
 
 console.log(`benim adim soyadim ${isim} ${soyisim} `);
 
+const sayi3 = "34";
+const sayi4 = 71;
 
-
-
-
-
-
+console.log(sayi3 + sayi4); //!3471
+console.log(sayi4 - sayi3); //!37
