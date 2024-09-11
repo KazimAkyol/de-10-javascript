b60049b1538d6ecd58b57dfbac8dd28d8b4bc03c
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-11 10:52:41 +0200

    5.commit

---
+++ javascript/class-notes/JS-01-Intro-Variables/app.js
@@ -0,0 +1,23 @@
+console.log("external js den selamlar");
+
+//yorum satiri
+
+console.log("burasi console alani");
+
+console.error("bu bir hata mesajidir");
+
+console.warn("bu bir uyari mesajidir");
+
+// console.clear()  //console temizlemek icin kullanilir
+
+//alert("alert mesaji")
+
+
+//PROMPT KULLANIMI
+
+console.log(prompt("lütfen adinizi giriniz"));
+
+
+
+
+
