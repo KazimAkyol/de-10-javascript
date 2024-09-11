4ea997b7aeac5862ffba0633e248a1b460233f59
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-11 12:25:56 +0200

    9.commit

---
+++ javascript/class-notes/JS-02-Operators/app.js
@@ -0,0 +1,54 @@
+// * =======================================================
+// *                 ARİTMETİK OPERATÖRLER
+// * =======================================================
+
+
+const cola=50
+const cips=35
+const dondurma=20
+
+console.log("cola" + cips + dondurma);
+
+let toplam=cola + cips + dondurma
+
+console.log(toplam);//105
+
+
+//! 1 artir
+
+toplam= toplam + 1
+
+console.log(toplam);//106
+
+toplam++
+
+console.log(toplam);//107
+
+//!10 arttir
+
+toplam=toplam+10
+
+toplam+=10 // toplam=toplam+10 kisa yolu
+
+console.log(toplam);//127
+
+
+//? + operatörü string concatination islemi de yapar
+
+const sayi1="12"
+const sayi2=45
+
+console.log(sayi1+sayi2);//1245
+console.log(sayi1-sayi2);//-33
+
+
+const isim="mehmet"
+const soyisim="kara"
+
+console.log("benim adim soyadim " +isim + "" +  soyisim);
+
+
+
+
+
+
