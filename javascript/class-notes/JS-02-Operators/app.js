2124c33f16cf422ff3a85be8c5f9a1c9272d5877
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-12 09:50:32 +0200

    13.commit

--- javascript/class-notes/JS-02-Operators/app.js
+++ javascript/class-notes/JS-02-Operators/app.js
@@ -52,3 +52,78 @@ const sayi4 = 71;
 
 console.log(sayi3 + sayi4); //!3471
 console.log(sayi4 - sayi3); //!37
+
+//?bazi fonksiyonlar
+
+// Math.floor();  //* her zaman en yakın alt tamsayıya yuvarlar
+// Math.ceil();  //* her zaman en yakın üst tam sayıya yuvarlar
+// Math.trunc(); //* sayının tam kısmını alır.
+// Math.round(); //* en yakın tam sayıya yuvarlar.
+
+// Math.random(); //* 0 ve 1 arasında rasgele sayı üretir.
+
+//toFixed(x) //*virgülden sonra x adet basamak bırak
+
+const ab=1.3
+const cd=1.8
+console.log(Math.floor(ab));
+console.log(Math.floor(cd));
+console.log(Math.ceil(ab));
+console.log(Math.ceil(cd));
+
+console.log(Math.trunc(ab));
+
+console.log(Math.round(ab));
+console.log(Math.round(cd));
+
+console.log((3.145674893939).toFixed(3));
+
+console.log(Math.random());
+
+
+//document.write(Math.trunc(ab));
+
+//RANDOM: rastgele
+//rasgele sayi üretmek icin kullanilir
+//0-1 arasinda bir sayi üretir
+console.log(Math.random());
+console.log(Math.ceil(Math.random()*20));
+
+
+//*ARTTIRMA AZALTMA
+
+let a=5
+
+console.log("ilk", a++);//5
+console.log("ikinci", a);//6
+
+let b=a++
+console.log(b);//&
+console.log(a);//7
+
+let c=10
+
+console.log("ilk", ++c);//11
+
+let d=++c
+console.log(d);//12
+console.log(c);//12
+
+let e=33
+
+console.log("ilk", e--);//33
+console.log("ikinci", e);//32
+
+console.log(--e);//31
+console.log(e);//31
+
+
+
+
+
+
+
+
+
+
+
