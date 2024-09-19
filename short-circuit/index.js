897b1d9f8dc29c91d865808ecbca67bcd8c0da64
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-19 11:09:27 +0200

    add commit

---
+++ short-circuit/index.js
@@ -0,0 +1,20 @@
+{
+  const a = 5;
+  console.log(a);
+}
+
+console.log("nedir");
+
+//Short Circuit
+
+const x = 5;
+const y = 8;
+
+if(x>0 && y>0) {
+    console.log("her iki kosulda dogru");
+}
+else{
+    console.log("en az bir kosul yanlis");
+}
+
+console.log("0" && [] && "merhaba"/5 && 404);
