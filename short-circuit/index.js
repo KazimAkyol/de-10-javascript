a2c9d32be2934f3e3968f7cad2737d86699f98b3
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-19 11:57:32 +0200

    add commit

--- short-circuit/index.js
+++ short-circuit/index.js
@@ -22,3 +22,14 @@ console.log("0" && [] && "merhaba"/5 && 404);
 for (let sayac = 1; sayac <= 10; sayac++) {
     console.log("clarusway");
 }
+
+let max = 5;
+let sum = 0;
+
+for (let counter = 0; counter <= max; counter++) {
+    sum = sum + counter;
+    console.log(sum);
+}
+
+console.log(sum);
+
