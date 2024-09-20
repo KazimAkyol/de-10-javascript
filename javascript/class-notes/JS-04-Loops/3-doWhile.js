c64066ead2485b70da03083a44926fbf6270e575
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-20 11:46:00 +0200

    add commit

--- javascript/class-notes/JS-04-Loops/3-doWhile.js
+++ javascript/class-notes/JS-04-Loops/3-doWhile.js
@@ -8,14 +8,22 @@
 
 //Kullanicinin sifresini kontrol eden program, dogru girene kadar soran bir program:
 
-let password = "1234.)";
+// let password = "1234.)";
 
-let userPassword;
+// let userPassword;
+
+// do {
+//   userPassword = prompt("Enter your a Password:");
+
+//   if (userPassword !== password) {
+//     console.log("Hatali Giris yaptiniz, Yeniden giriniz");
+//   }
+// } while (userPassword !== password);
+
+//Kullanicidan sayi istiyoruz. Sayi girilmediyse tekrar girsin
+
+let sayi;
 
 do {
-  userPassword = prompt("Enter your a Password:");
-
-  if (userPassword !== password) {
-    console.log("Hatali Giris yaptiniz, Yeniden giriniz");
-  }
-} while (userPassword !== password);
+  sayi = prompt("Bir sayi girin:");
+} while (isNaN(sayi));
