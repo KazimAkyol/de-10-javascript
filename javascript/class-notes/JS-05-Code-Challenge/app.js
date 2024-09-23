a61d262dbabb96e7e512d1b586c33507cc860e8b
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-23 11:07:21 +0200

    add commit

--- javascript/class-notes/JS-05-Code-Challenge/app.js
+++ javascript/class-notes/JS-05-Code-Challenge/app.js
@@ -7,23 +7,23 @@ console.log("***** Code Class - Loops *****");
 
 //? her iki degerde true ise son true degeri döndürür, aksi takdirde ilk false degeri döndürür.
 
-console.log(true && 1);// 1
-console.log(false && 1);// false
-console.log(1 && 3);//3
-console.log(0 && 1);// 0
-console.log(NaN && 1);//NaN
+console.log(true && 1); // 1
+console.log(false && 1); // false
+console.log(1 && 3); //3
+console.log(0 && 1); // 0
+console.log(NaN && 1); //NaN
 console.log([] && 1);
-console.log(Boolean([]));//true
+console.log(Boolean([])); //true
 
 //OR OPERATÖRÜ
 console.log("***OR***");
-console.log(true || false);//true
-console.log( 1 || false);//1
-console.log(0 || NaN);//NaN
-console.log(213 || "fatma" || true);//213
-console.log([] || "" || undefined);//[]
-console.log(0 || "" || undefined);//undefined
-console.log(0 || false || undefined);//undefined
+console.log(true || false); //true
+console.log(1 || false); //1
+console.log(0 || NaN); //NaN
+console.log(213 || "fatma" || true); //213
+console.log([] || "" || undefined); //[]
+console.log(0 || "" || undefined); //undefined
+console.log(0 || false || undefined); //undefined
 
 // const sabah = false
 
@@ -34,18 +34,15 @@ console.log(0 || false || undefined);//undefined
 //     console.log("Güle Güle");
 // }
 
-
-
-
 //! 18 yasindan büyüklerin oy kullanabilecegini 18 yasindan kücüklerin oy kullanamayacagini ternary yapi ile konsola yazdiran kodu yaziniz. (ternary)
 
 //let yas = +prompt("yasinizi giriniz");
 
 //if(yas < 18) {
-    //console.log("Yasiniz 18'den kücüktür.Oy kullanamazsiniz");
+//console.log("Yasiniz 18'den kücüktür.Oy kullanamazsiniz");
 //}
 //else{
-    //console.log("Yasiniz 18'den büyüktür.Oy kullanabilirsiniz");
+//console.log("Yasiniz 18'den büyüktür.Oy kullanabilirsiniz");
 //}
 
 ///let yas = +prompt("yasinizi giriniz");
@@ -76,39 +73,57 @@ console.log(0 || false || undefined);//undefined
 //? - 30'den büyük yada 40'a eşitse DD,
 //? - 30'dan küçük yada eşitse FF,
 
-let x = +prompt("Lütfen notunuzu giriniz");
+// let x = +prompt("Lütfen notunuzu giriniz");
 
-if (x<0 || x>100) {
-    console.log("Lütfen 1-100 arasinda bir deger giriniz");
+// if (x<0 || x>100) {
+//     console.log("Lütfen 1-100 arasinda bir deger giriniz");
+// }
+// else if (x>90) {
+//     console.log("AA");
+// }
+// else if (x>80) {
+//     console.log("AB");
+// }
+// else if (x>70) {
+//     console.log("BB");
+// }
+// else if (x>60) {
+//     console.log("BC");
+// }
+// else if (x>50) {
+//     console.log("CC");
+// }
+// else if (x>40) {
+//     console.log("DD");
+// }
+// else {
+//     console.log("FF");
+// }
+
+//! 3 - Artik yil sorusu
+
+//? Artık yıl, bir yıldaki günlerin 365 yerine 366 gün olmasına denir. Bu fazladan gün (artık gün), normalde 28 gün olan şubat ayına 29 Şubat'ın eklenmesi ile elde edilir.  Bunun hesaplanması için aşağıdaki algoritma kullanılır.
+
+//? Bir tarih 4 ile tam bölünüp 100 katları ile tam bölünemiyorsa, veya yıl 400 sayısına tam bölünüyorsa artık yıldır. BU iki şartın dışında yılarda şubat ayı 28 gün, artık yıllarda 29 gün olarak belirlenir.
+
+//? Yukarıdaki bilgilere göre bir yılın artık yıl olup olmadığını konsola yazdıran conditional statement'i yazınız.
+
+// let y = 2001;
+
+// if (y%4 === 0 && y%100 !== 0) {
+//     console.log(`${y} yili artik yildir`);
+// }
+// else if (y % 400 = 000) {
+//     console.log(`${y} yili artik yildir`);
+// }
+// else{
+//     console.log(`${y} yili artik yil degildir`);
+// }
+
+//! 4 - 1000 ile 2000 arasindaki 13'e bölümünden kalani 3 olan sayilari console a yazdiriniz
+
+for (let i = 1000; i < 2000; i++) {
+  if (i % 13 == 3) {
+    console.log(i);
+  }
 }
-else if (x>90) {
-    console.log("AA");
-}
-else if (x>80) {
-    console.log("AB");
-}
-else if (x>70) {
-    console.log("BB");
-}
-else if (x>60) {
-    console.log("BC");
-}
-else if (x>50) {
-    console.log("CC");
-}
-else if (x>40) {
-    console.log("DD");
-}
-else {
-    console.log("FF");
-}
-
-
-
-
-
-
-
-
-
-
